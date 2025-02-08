0648b750d57bf6c66794790c5a2b3746cf5c19ae
Krrish Gupta <2024.krrish.gupta@ves.ac.in>
2025-02-08 12:24:22 +0530

    Added power function

--- a.cpp
+++ a.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
 
+int pow(int x, int n) {
+    if(n == 1) return n;
+
+    int res = pow(x*x, int(n/2));
+
+    if(n&1) return res * res * n;
+    else return res * res;
+}
+
 void print(string s) {
     cout << s << endl;
 }
